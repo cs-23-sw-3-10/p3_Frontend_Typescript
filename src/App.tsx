2f1fece4864a561411c8d6fe595d92c40692527f
EmilChri <113058804+EmilChri@users.noreply.github.com>
2023-11-01 11:45:47 +0100

    max zoom er nu 14 dage (kan nemt ændres)

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import moment from 'moment'
 import 'react-calendar-timeline/lib/Timeline.css'
 
 function App() {
-  const groups = [{ id: 1, title: 'rig 1', stackItems: false, height: 30 }, { id: 2, title: 'rig 2' }, { id: 3, title: 'rig 3' }, { id: 4, title: 'rig 4' }, { id: 5, title: 'rig 5' }, { id: 6, title: 'rig 6' }]
-  const items = [
+  const testRigs = [{ id: 1, title: 'rig 1', stackItems: false, height: 30 }, { id: 2, title: 'rig 2' }, { id: 3, title: 'rig 3' }, { id: 4, title: 'rig 4' }, { id: 5, title: 'rig 5' }, { id: 6, title: 'rig 6' }]
+  const bladeTasks = [
     {
       id: 1,
-      group: 1,
+      group: 1, 
       title: 'den nye',
       start_time: moment().year(2023).month(10).date(1),
       end_time:moment().year(2023).month(10).date(6),
@@ -24,13 +24,12 @@ function App() {
     <div>
       <h1>Scrollable Horizontal Schedule</h1>
       <Timeline
-        groups={groups}
-        items={items}
-        defaultTimeStart={moment()}
-        defaultTimeEnd={moment().add(30, 'days')}
-        // visibleTimeStart={moment().subtract(7, 'days')}
-        // visibleTimeEnd={moment().add(7, 'days')}
-        timeSteps={{
+        groups={testRigs}
+        items={bladeTasks}
+        minZoom={60 * 60 * 24 * 14 * 1000} // 14 days is the smallest zoom
+        defaultTimeStart={moment()} // the timeline will start today
+        defaultTimeEnd={moment().add(30, 'days')} // and will end 30 days from today
+        timeSteps={{ // zooming will be at these intervals
           second: 0,
           minute: 0,
           hour: 0,
