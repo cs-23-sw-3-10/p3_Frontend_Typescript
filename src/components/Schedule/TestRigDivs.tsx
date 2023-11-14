a6d1b3912a08ebcaacc0b85311c145f886ddc991
EmilChri <113058804+EmilChri@users.noreply.github.com>
2023-11-14 09:07:48 +0100

    Update TestRigDivs.tsx

--- src/components/Schedule/TestRigDivs.tsx
+++ src/components/Schedule/TestRigDivs.tsx
@@ -1,28 +1,20 @@
-// import { viewChange } from "./Display";
-import { is } from "@babel/types";
-import React, { SetStateAction, Dispatch , useState} from "react";
+import React, { SetStateAction, Dispatch } from "react";
 
-
-function CreateTestRigDivs(rigs: string[], dates: Date[], setDates: Dispatch<SetStateAction<Date[]>>) {
-
-    
-
+function CreateTestRigDivs(
+    rigs: string[],
+    dates: Date[],
+    setDates: Dispatch<SetStateAction<Date[]>>
+) {
     // Create a div for each test rig
     return (
         <div className="TestRigOverviewContainer">
-            <div className="TestRigDateHeaderSpace">
-                
-            </div>
-            
+            <div className="TestRigDateHeaderSpace"></div>
+
             {rigs.map((rig) => (
-                <div className="TestRigOverviewElement"
-                    key={rig}
-                    id={rig}>
+                <div className="TestRigOverviewElement" key={rig} id={rig}>
                     <h4>{rig}</h4>
                 </div>
-            ))
-            }
-           
+            ))}
         </div>
     );
 }
