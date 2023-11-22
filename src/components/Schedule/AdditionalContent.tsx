4ecf03dd9370dcc096da1ad486e60ae18df3e4ee
Andreas Valentin Pedersen <andreasvalentin@hotmail.com>
2023-11-22 15:42:38 +0100

    click handlers

--- src/components/Schedule/AdditionalContent.tsx
+++ src/components/Schedule/AdditionalContent.tsx
@@ -1,26 +1,53 @@
+import { useState } from "react";
 import StyledButton from "../ui/styledButton";
 
 function CreateAdditionalContent() {
+    const [createTasksAndProjects, setCreateTasksAndProjects] = useState("showButtons");
+
+    const handleCreateProjectClick=()=>{
+        console.log("button was clicked")
+        setCreateTasksAndProjects("createProject")
+    }
+
+    const handleCreateTaskClick=()=>{
+        console.log("button was clicked")
+        setCreateTasksAndProjects("createTask")
+    }
+
     return (
         <div className="AdditionalContent">
+            {createTasksAndProjects==="showButtons" &&
             <div className="buttonContainer">
                 <StyledButton
                     style={{ marginRight: "10px" }}
-                    onClick={() => {
-                        console.log("button was clicked");
-                    }}
+                    onClick={handleCreateProjectClick}
                 >
                     Create Blade Project
                 </StyledButton>
-                <StyledButton
-                    onClick={() => {
-                        console.log("button was clicked");
-                    }}
-                >
+                <StyledButton onClick={handleCreateTaskClick}>
                     Create Blade Task
                 </StyledButton>
             </div>
+            }
+            {createTasksAndProjects==="createTask" && <DummyCreateTask />}
+            {createTasksAndProjects==="createProject" && <DummyCreateProject />}
         </div>
     );
 }
 export default CreateAdditionalContent;
+
+function DummyCreateTask() {
+    return (
+        <div style={{ height: "1000px", width: "600px", background: "blue" }}>
+            Dummy create BT componenent
+        </div>
+    );
+}
+
+function DummyCreateProject() {
+    return (
+        <div style={{ height: "500px", width: "300px", background: "red" }}>
+            Dummy create BT componenent
+        </div>
+    );
+}
