d118259a56722bacfba58b1f6252e65e99e1916a
Andreas Valentin Pedersen <andreasvalentin@hotmail.com>
2023-11-22 14:48:54 +0100

    css and onclick

--- src/components/Schedule/AdditionalContent.tsx
+++ src/components/Schedule/AdditionalContent.tsx
@@ -3,10 +3,24 @@ import StyledButton from "../ui/styledButton";
 function CreateAdditionalContent() {
     return (
         <div className="AdditionalContent">
-            <StyledButton>Create Blade Project</StyledButton>
-            <StyledButton>Create Blade Task</StyledButton>
+            <div className="buttonContainer">
+                <StyledButton
+                    style={{ marginRight: "10px" }}
+                    onClick={() => {
+                        console.log("button was clicked");
+                    }}
+                >
+                    Create Blade Project
+                </StyledButton>
+                <StyledButton
+                    onClick={() => {
+                        console.log("button was clicked");
+                    }}
+                >
+                    Create Blade Task
+                </StyledButton>
+            </div>
         </div>
     );
 }
 export default CreateAdditionalContent;
-
