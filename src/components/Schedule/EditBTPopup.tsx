765aed3e04f8e490abcf5ca4e5182ebcc40114c9
SstormM <sebbemain@gmail.com>
2023-11-28 21:40:00 +0100

    Final clean up

--- src/components/Schedule/EditBTPopup.tsx
+++ src/components/Schedule/EditBTPopup.tsx
@@ -1,48 +1,15 @@
-import React, { useState } from 'react';
-import BladeTaskMenu from "../CreateBTMenu/BladeTaskMenu";
-
 type EditBTPopupProps = {
     onClose: () => void;
-};
-
-function EditBTPopup(props: EditBTPopupProps) {
-    // State to store the position
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [dragging, setDragging] = useState(false);
-
-    // Function to update position on drag
-    const onDrag = (e:any) => {
-        if (dragging) {
-            setPosition({
-                x: position.x + e.movementX,
-                y: position.y + e.movementY
-            });
-        }
-    };
-
-    // Start dragging
-    const startDrag = () => setDragging(true);
-
-    // Stop dragging
-    const stopDrag = () => setDragging(false);
-
-    return (
-        <div 
-            className="EditBTPopup" 
-            onMouseMove={onDrag}
-            onMouseUp={stopDrag}
-            onMouseLeave={stopDrag}
-            style={{ position: 'fixed', left: `${position.x}px`, top: `${position.y}px` }}
-            
-        >
-            <div className="EditBTPopupContent" >
-                <BladeTaskMenu/>
-                
-            </div>
-            <button className="EditBTPopupCloseButton" onClick={props.onClose}>Close</button>
-            <div className='dragField' onMouseDown={startDrag}></div>
-        </div>
-    );
 }
 
+function EditBTPopup(props: EditBTPopupProps){
+    return(
+        <div className="EditBTPopup">
+            <div className="EditBTPopupContent">
+            <h1>Edit BT Popup</h1>
+            </div>
+             <button className="EditBTPopupCloseButton" onClick={props.onClose}>Close</button>
+        </div>
+    )
+}
 export default EditBTPopup;
