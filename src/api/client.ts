ee9a72589f04a921558e409681d4d99ccc48409b
sebub <ssma22@student.aau.dk>
2023-11-29 14:22:50 +0100

    dt

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,11 +3,26 @@ import { createClient } from 'graphql-ws';
 import { split, HttpLink } from '@apollo/client';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
+// const authLink = setContext(() => {
+//     // get the authentication token from local storage if it exists
+//     const token = localStorage.getItem('token');
+//     // return the headers to the context so httpLink can read them
+//     return {
+//       headers: {
+//         authorization: token ? `Bearer ${token}` : "",
+//       }
+//     }
+//   });
 
 
 const httpLink = new HttpLink({
+
   uri: 'http://localhost:8080/graphql',
 });
+
 export const wsLink = new GraphQLWsLink(createClient({
   url: 'ws://localhost:8080/graphql',
 }
