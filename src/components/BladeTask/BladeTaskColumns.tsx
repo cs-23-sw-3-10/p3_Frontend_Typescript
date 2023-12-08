5609c56a704315056b212947c737d8de417607dc
Andreas Valentin Pedersen <andreasvalentin@hotmail.com>
2023-12-09 00:29:31 +0100

    Update BladeTaskColumns.tsx

--- src/components/BladeTask/BladeTaskColumns.tsx
+++ src/components/BladeTask/BladeTaskColumns.tsx
@@ -6,12 +6,7 @@ export const columnBT: ColumnDef<BladeTaskQuery>[] = [
     {
         header: ({ column }) => {
             return (
-                <button
-                    onClick={() =>
-                        column.toggleSorting(column.getIsSorted() === "asc")
-                    }
-                    style={{ fontWeight: 'bold' }}
-                >
+                <button onClick={() => column.toggleSorting(column.getIsSorted() === "asc")} style={{ fontWeight: "bold" }}>
                     BT Name
                 </button>
             );
@@ -21,9 +16,7 @@ export const columnBT: ColumnDef<BladeTaskQuery>[] = [
             return (
                 <>
                     {row.getCanExpand() ? (
-                        <button>
-                            {row.getIsExpanded() ? "▲" : "▼"}
-                        </button>
+                        <button>{row.getIsExpanded() ? "▲" : "▼"}</button>
                     ) : (
                         <button
                             {...{
@@ -43,99 +36,43 @@ export const columnBT: ColumnDef<BladeTaskQuery>[] = [
     },
     {
         header: ({ column }) => {
-            return (
-                <button
-                    onClick={() =>
-                        column.toggleSorting(column.getIsSorted() === "asc")
-                    }   
-                >
-                    Test Type
-                </button>
-            );
+            return <button onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>Test Type</button>;
         },
         accessorKey: "testType",
     },
     {
         header: ({ column }) => {
-            return (
-                <button
-                    onClick={() =>
-                        column.toggleSorting(column.getIsSorted() === "asc")
-                    }
-                >
-                    Start Date
-                </button>
-            );
+            return <button onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>Start Date</button>;
         },
         accessorKey: "startDate",
     },
     {
         header: ({ column }) => {
-            return (
-                <button
-                    onClick={() =>
-                        column.toggleSorting(column.getIsSorted() === "asc")
-                    }
-                >
-                    End Date
-                </button>
-            );
+            return <button onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>End Date</button>;
         },
         accessorKey: "endDate",
     },
     {
         header: ({ column }) => {
-            return (
-                <button
-                    onClick={() =>
-                        column.toggleSorting(column.getIsSorted() === "asc")
-                    }
-                >
-                    Duration
-                </button>
-            );
+            return <button onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>Duration</button>;
         },
         accessorKey: "duration",
     },
     {
         header: ({ column }) => {
-            return (
-                <button
-                    onClick={() =>
-                        column.toggleSorting(column.getIsSorted() === "asc")
-                    }
-                >
-                    Attach Period
-                </button>
-            );
+            return <button onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>Attach Period</button>;
         },
         accessorKey: "attachPeriod",
     },
     {
         header: ({ column }) => {
-            return (
-                <button
-                    onClick={() =>
-                        column.toggleSorting(column.getIsSorted() === "asc")
-                    }
-                >
-                    Detach Period
-                </button>
-            );
+            return <button onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>Detach Period</button>;
         },
         accessorKey: "detachPeriod",
     },
     {
         header: ({ column }) => {
-            return (
-                <button
-                    onClick={() =>
-                        column.toggleSorting(column.getIsSorted() === "asc")
-                    }
-                >
-                    Test Rig 
-                </button>
-            );
+            return <button onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>Test Rig</button>;
         },
         accessorKey: "testRig",
     },
