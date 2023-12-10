d298dca61f2c016fbdfbda4337d0b0652d3297e9
Adomas Ciplys <aciply22@student.aau.dk>
2023-12-10 13:33:16 +0100

    Added comments to validate function

--- src/components/CreateBTMenu/ValidateForm.tsx
+++ src/components/CreateBTMenu/ValidateForm.tsx
@@ -6,15 +6,16 @@ import { InErrorChart } from "./BTMenuTypes";
 //Required Fields: taskName, testType, bladeProjectID, duration, attachPeriod, detachPeriod
 export function ValidateForm(currentOrder:BTOrder, inErrorChart:InErrorChart, setInErrorChart:Function, BTNames:Array<string>){
     let newInErrorChart = {...inErrorChart};
-    newInErrorChart.taskName[0] = (BTNames.includes(currentOrder.taskName)); //Checks if BT name exist in BP already
-    newInErrorChart.taskName[1] = (currentOrder.taskName === "");
-    newInErrorChart.testType = (currentOrder.testType === "");
-    newInErrorChart.bladeProjectId = (currentOrder.bladeProjectId === "");
-    newInErrorChart.duration[1] = (currentOrder.duration >= 0);
-    newInErrorChart.attachPeriod[1] = (currentOrder.attachPeriod >= 0);
-    newInErrorChart.detachPeriod[1] = (currentOrder.detachPeriod >= 0);
-    setInErrorChart(newInErrorChart)
+    newInErrorChart.taskName[0] = (BTNames.includes(currentOrder.taskName)); //Checks if Task Name exist in BP already
+    newInErrorChart.taskName[1] = (currentOrder.taskName === ""); //Checks if Task Name is not empty
+    newInErrorChart.testType = (currentOrder.testType === ""); //Cheks if Test Type is not empty
+    newInErrorChart.bladeProjectId = (currentOrder.bladeProjectId === ""); //Cheks if BP id is provided
+    newInErrorChart.duration[1] = (currentOrder.duration <= 0); //Checks if durations is provided and not negative
+    newInErrorChart.attachPeriod[1] = (currentOrder.attachPeriod <= 0); //Checks if attach period is provided and not negative
+    newInErrorChart.detachPeriod[1] = (currentOrder.detachPeriod <= 0); //Checks if detach period is provided and not negative
+    setInErrorChart(newInErrorChart) //Update error chart
     
+    //If all statements above are false -> Create BT
     if(newInErrorChart.taskName[0] || newInErrorChart.taskName[1] || newInErrorChart.testType ||  
         newInErrorChart.bladeProjectId ||    newInErrorChart.duration[1] || 
         newInErrorChart.attachPeriod[1]  ||  newInErrorChart.detachPeriod[1])
