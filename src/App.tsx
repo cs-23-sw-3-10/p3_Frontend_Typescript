714dd8f1bf8a8f894b66390624fc008415b9757f
Peterrahr <Prni22@student.aau.dk>
2023-12-11 20:04:45 +0100

    Ændret EditModeProvider
    Ændret placeringen af EditModeProvider, da det er bedre sat op ift code sample til rapport.

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,11 @@ import "./App.css";
 import ScheduleComponent from "./components/Schedule/ScheduleComponent";
 import BTPage from "./components/BladeTask/BladeTaskPage";
 import BladeProjectPage from "./components/BladeProject/BladeProjectPage";
-import { EditModeProvider } from "./EditModeContext";
 
 function App() {
     return (
         <>
             <div className="app-container">
-                <EditModeProvider>
                     <NavBar />
                     <div className="content-container">
                         <Routes>
@@ -20,11 +18,9 @@ function App() {
                             <Route path="/projects" element={<BladeProjectPage />} />
                             <Route path="/bladetask" element={<BTPage />} />
                             <Route path="/resources" element={<ResourceTable />} />
-
                             <Route path="*" element={<Navigate to="/" />} />
                         </Routes>
                     </div>
-                </EditModeProvider>
             </div>
         </>
     );
