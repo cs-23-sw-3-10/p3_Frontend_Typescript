da91fe3b560ba4c2cd6f2855eb8a390fb7d10ecd
EmilChri <113058804+EmilChri@users.noreply.github.com>
2023-12-14 09:54:32 +0100

    den rigtige længde af attach og detach vises hvis BT går ud over view

--- src/components/Schedule/RigFieldContainer.tsx
+++ src/components/Schedule/RigFieldContainer.tsx
@@ -30,6 +30,7 @@ function CreateRigFieldContainer(props: RigFieldContainerProps) {
         minHeight: `${rigFieldHeight}px`,
         maxHeight: `${rigFieldHeight}px`,
     };
+    let BTsToRender: React.ReactNode[] = []; // BladeTaskCards to be rendered
     let BTsStartInView: React.ReactNode[] = []; // BladeTaskCards that start in the view
     let BTsEndInView: React.ReactNode[] = []; // BladeTaskCards that end in the view
     let BTsLongerThanView: React.ReactNode[] = []; // BladeTaskCards that are longer than the view
@@ -39,22 +40,47 @@ function CreateRigFieldContainer(props: RigFieldContainerProps) {
             (bladeTask as React.ReactElement<any>).props.endDate > props.viewMonths[props.viewMonths.length - 1]
         ) {
             BTsLongerThanView.push(bladeTask); // Add the BladeTaskCard to the array if the startDate is in the view
-        }else if (
+        } else if (
             (bladeTask as React.ReactElement<any>).props.startDate >= props.viewMonths[0] &&
             (bladeTask as React.ReactElement<any>).props.startDate <= props.viewMonths[props.viewMonths.length - 1]
         ) {
-            BTsStartInView.push(bladeTask); // Add the BladeTaskCard to the array if the startDate is in the view
+            if ((bladeTask as React.ReactElement<any>).props.endDate <= props.viewMonths[props.viewMonths.length - 1]) {
+                BTsToRender.push(bladeTask); // Add the BladeTaskCard to the array if the startDate is in the view
+            } else {
+                BTsStartInView.push(bladeTask); // Add the BladeTaskCard to the array if the startDate is in the view
+            }
         } else if (
             (bladeTask as React.ReactElement<any>).props.endDate >= props.viewMonths[0] &&
             (bladeTask as React.ReactElement<any>).props.endDate <= props.viewMonths[props.viewMonths.length - 1]
         ) {
             BTsEndInView.push(bladeTask); // Add the BladeTaskCard to the array if the endDate is in the view
         }
-        
     });
 
+    BTsStartInView.forEach((bladeTask) => {
+        BTsToRender.push(
+            // Add the BladeTaskCard to the array if the startDate is in the view
+            <BladeTaskCard
+                key={(bladeTask as React.ReactElement<any>).props.id}
+                id={(bladeTask as React.ReactElement<any>).props.id}
+                rig={(bladeTask as React.ReactElement<any>).props.rig}
+                startDate={(bladeTask as React.ReactElement<any>).props.startDate}
+                endDate={props.viewMonths[props.viewMonths.length - 1]}
+                duration={(bladeTask as React.ReactElement<any>).props.duration}
+                projectColor={(bladeTask as React.ReactElement<any>).props.projectColor}
+                projectId={(bladeTask as React.ReactElement<any>).props.projectId}
+                customer={(bladeTask as React.ReactElement<any>).props.customer}
+                taskName={(bladeTask as React.ReactElement<any>).props.taskName}
+                attachPeriod={(bladeTask as React.ReactElement<any>).props.attachPeriod}
+                detachPeriod={calcDetachLength(bladeTask as React.ReactElement<any>, props.viewMonths[props.viewMonths.length - 1])}
+                shown={editMode ? true : false}
+                enableDraggable={editMode ? true : false}
+                enableContextMenu={editMode ? true : false}
+            />
+        );
+    });
     BTsEndInView.forEach((bladeTask) => {
-        BTsStartInView.push(
+        BTsToRender.push(
             // Add the BladeTaskCard to the array if the endDate is in the view
             <BladeTaskCard
                 key={(bladeTask as React.ReactElement<any>).props.id}
@@ -67,15 +93,16 @@ function CreateRigFieldContainer(props: RigFieldContainerProps) {
                 projectId={(bladeTask as React.ReactElement<any>).props.projectId}
                 customer={(bladeTask as React.ReactElement<any>).props.customer}
                 taskName={(bladeTask as React.ReactElement<any>).props.taskName}
-                attachPeriod={(bladeTask as React.ReactElement<any>).props.attachPeriod}
+                attachPeriod={calcAttachLength(bladeTask as React.ReactElement<any>, props.viewMonths[0])}
                 detachPeriod={(bladeTask as React.ReactElement<any>).props.detachPeriod}
                 shown={editMode ? true : false}
                 enableDraggable={editMode ? true : false}
+                enableContextMenu={editMode ? true : false}
             />
         );
     });
     BTsLongerThanView.forEach((bladeTask) => {
-        BTsStartInView.push(
+        BTsToRender.push(
             <BladeTaskCard
                 key={(bladeTask as React.ReactElement<any>).props.id}
                 id={(bladeTask as React.ReactElement<any>).props.id}
@@ -87,10 +114,11 @@ function CreateRigFieldContainer(props: RigFieldContainerProps) {
                 projectId={(bladeTask as React.ReactElement<any>).props.projectId}
                 customer={(bladeTask as React.ReactElement<any>).props.customer}
                 taskName={(bladeTask as React.ReactElement<any>).props.taskName}
-                attachPeriod={(bladeTask as React.ReactElement<any>).props.attachPeriod}
-                detachPeriod={(bladeTask as React.ReactElement<any>).props.detachPeriod}
+                attachPeriod={calcAttachLength(bladeTask as React.ReactElement<any>, props.viewMonths[0])}
+                detachPeriod={calcDetachLength(bladeTask as React.ReactElement<any>, props.viewMonths[props.viewMonths.length - 1])}
                 shown={editMode ? true : false}
                 enableDraggable={editMode ? true : false}
+                enableContextMenu={editMode ? true : false}
             />
         );
     });
@@ -108,7 +136,7 @@ function CreateRigFieldContainer(props: RigFieldContainerProps) {
                     />
                 ) // Create a date for each day in the month
             )}
-            {BTsStartInView}
+            {BTsToRender}
             {/*automatically spreads out the entries of BladeTaskCards */}
         </div>
     );
@@ -122,9 +150,12 @@ function getRigDateKey(rig: string, date: Date) {
 function getDurationWhenLongerThanView(months: Date[]) {
     let duration = 0;
     months.forEach((month) => {
-        duration += getMonthLength(capitalizeFirstLetter(
-            month.toLocaleString("default", { month: "long" }) // Get the month name
-        ), month.getFullYear());
+        duration += getMonthLength(
+            capitalizeFirstLetter(
+                month.toLocaleString("default", { month: "long" }) // Get the month name
+            ),
+            month.getFullYear()
+        );
     });
     return duration;
 }
@@ -142,11 +173,41 @@ function getDurationEndInView(BT: React.ReactElement<any>, months: Date[]) {
         } else if (month > endDate) {
             duration += 0;
         } else {
-            duration += getMonthLength(capitalizeFirstLetter(
-                month.toLocaleString("default", { month: "long" }) // Get the month name
-            ), month.getFullYear());
+            duration += getMonthLength(
+                capitalizeFirstLetter(
+                    month.toLocaleString("default", { month: "long" }) // Get the month name
+                ),
+                month.getFullYear()
+            );
         }
     });
-
     return duration;
+}
+
+function calcAttachLength(bladeTask: React.ReactElement<any>, date: Date) {
+    let attachPeriod = bladeTask.props.attachPeriod;
+    let startDate = bladeTask.props.startDate;
+    let attachLength = 0;
+    let dayDiff = 0;
+    if (attachPeriod > 0) {
+        dayDiff = Math.ceil((date.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
+        if (dayDiff < attachPeriod) {
+            attachLength = attachPeriod - dayDiff;
+        }
+    }
+    return attachLength;
+}
+
+function calcDetachLength(bladeTask: React.ReactElement<any>, date: Date) {
+    let detachPeriod = bladeTask.props.detachPeriod;
+    let endDate = bladeTask.props.endDate;
+    let detachLength = 0;
+    let dayDiff = 0;
+    if (detachPeriod > 0) {
+        dayDiff = Math.ceil((endDate.getTime() - date.getTime()) / (1000 * 3600 * 24));
+        if (dayDiff < detachPeriod) {
+            detachLength = detachPeriod - dayDiff;
+        }
+    }
+    return detachLength;
 }
